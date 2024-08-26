5afbb3a1833b0c3043553cdadfec73767a2e4a97
wtwo1529 <003axelw@gmail.com>
2024-08-27 01:52:41 +0800

    Moved all modal handling js functions into a class, need to split it up and better follow SOLID principles. Additionally, make form submit json of form data and form validation user feedback

--- src/handleModal.js
+++ src/handleModal.js
@@ -1,38 +1,95 @@
-let getFormData = (valueHash) => {
-    let taskName = document.querySelector('#task-name-input').value;
-    let dateTime = document.querySelector('#date-input').value;
-    let priority = document.querySelector('input[name="priority-radio-btn"]:checked').value;
-    let json = {
-        "task": taskName,
-        "datetime": dateTime,
-        "priority": priority
+class handleModal {
+    constructor(inputElements) {
+        this.setShowModalRoot();
+        this.setClickModal();
+        this.submitForm();
+        this.setRemoveEmptyClassInput(inputElements);
     }
-    return json;
-}
-let validateForm = () => {
-    let nonRadioInputs = document.querySelectorAll('input:not([type="radio"])');
-    let priority = document.querySelector('input[name="priority-radio-btn"]:checked');
-    nonRadioInputs.forEach(element => {
-        if (typeof element.value == "string") {
-            let value = element.value.trim();
-            if (value == '' || value == "Task name") {
-                element.classList.add('empty-field');
-            }
-        }
-        else {
-            if (element.value == '') {
-                element.classList.add('empty-field');
-            }
-            console.log(element);
-        }
-    })
-};
-
-let removeEmptyClassInput = (inputElements) => {
-   inputElements.forEach(element => {
-        element.addEventListener('focus', () => {
-            element.classList.remove('empty-field');
+    setShowModalRoot() {
+        let doAddBtns = document.querySelectorAll('.do-list-add-task-btn');
+        doAddBtns.forEach((addBtn) => {
+            addBtn.addEventListener('click', this.openModal)
         })
-   }); 
+
+        document.querySelector('#modal-root').addEventListener('click', this.closeModal);
+    }
+    openModal() {
+        document.querySelector('#modal').classList.add('modal');
+                document.querySelector('#modal-root').classList.add('modal-root'); 
+    }
+    closeModal() {
+        document.querySelector('#modal-root').classList.remove('modal-root');
+        document.querySelector('#modal').classList.remove('modal');
+    }
+    clearInputs() {
+        document.querySelectorAll('input:not([type="radio"])').forEach(
+            (input) => {
+                input.value = '';
+            }
+        )
+        let priority = document.querySelector('input[name="priority-radio-btn"]:checked');
+        if (priority) {
+            priority.checked = false;
+        }
+        document.querySelector('#task-name-input').value = 'Task name';
+    }
+    setClickModal() {
+        document.querySelector('#modal').addEventListener('click', (event) => {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+            return false;
+        })
+    }
+    getFormData() {
+        let taskName = document.querySelector('#task-name-input').value;
+        let dateTime = document.querySelector('#date-input').value;
+        let priority = document.querySelector('input[name="priority-radio-btn"]:checked') ?? false;
+        let json = {
+            "task": taskName,
+            "datetime": dateTime,
+            "priority": priority.value || "-1"
+        }
+        return json;
+    };
+    submitForm() {
+        document.querySelector('#add-task-form').addEventListener('submit', (event) => {
+            event.preventDefault();
+            if (this.validateForm()) {
+                this.closeModal();
+            }
+            let formData = this.getFormData();
+            this.clearInputs();
+            console.log(formData);
+        });
+    }
+    validateForm() {
+        let nonRadioInputs = document.querySelectorAll('input:not([type="radio"])');
+        let priority = document.querySelector('input[name="priority-radio-btn"]:checked');
+        let valid = true;
+        nonRadioInputs.forEach(element => {
+            if (typeof element.value == "string") {
+                let value = element.value.trim();
+                if (value == '' || value == "Task name") {
+                    element.classList.add('empty-field');
+                    valid = false;
+                }
+            }
+            else {
+                if (element.value == '') {
+                    element.classList.add('empty-field');
+                    valid = false;
+                }
+            }
+        })
+        return valid;
+    };
+    
+    setRemoveEmptyClassInput(inputElements) {
+        inputElements.forEach(element => {
+            element.addEventListener('focus', () => {
+                element.classList.remove('empty-field');
+            })
+       }); 
+    }
 }
-export {getFormData, validateForm, removeEmptyClassInput};
+export default handleModal;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./style.css"; 
 
-import { default as getFormData } from './handleModal';
+import { default as handleModal } from './handleModal';
 
 window.onload = () => {
-    
+    let inputElements = document.querySelectorAll('input:not([type="radio"])');
     let taskNameInput = document.querySelector('#task-name-input');
     taskNameInput.value = 'Task name';
     taskNameInput.addEventListener("focus", (event) => {
@@ -18,22 +18,6 @@ window.onload = () => {
         }
     })
 
-    let doAddBtns = document.querySelectorAll('.do-list-add-task-btn');
-    doAddBtns.forEach((addBtn) => {
-        addBtn.addEventListener('click', () => {
-            document.querySelector('#modal').classList.add('modal');
-            document.querySelector('#modal-root').classList.add('modal-root'); 
-        })
-    })
-    document.querySelector('#modal-root').addEventListener('click', (event) => {
-        document.querySelector('#modal-root').classList.remove('modal-root');
-        document.querySelector('#modal').classList.remove('modal');
-    })
-    document.querySelector('#modal').addEventListener('click', (event) => {
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-        return false;
-    })
-
+    let modalLogic = new handleModal(inputElements);
     // document.querySelector('#modal-submit').addEventListener('click', getFormData);
 }
