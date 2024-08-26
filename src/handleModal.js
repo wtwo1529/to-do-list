3ff6e2853374a9e0f025f70762c8c342151b6936
wtwo1529 <003axelw@gmail.com>
2024-08-26 19:12:43 +0800

    Start modal form js file

--- src/handleModal.js
+++ src/handleModal.js
@@ -9,4 +9,15 @@ let getFormData = () => {
     }
     return json;
 }
+let validateForm = () => {
+    let taskName = document.querySelector('#task-name-input')
+    let dateTime = document.querySelector('#date-input')
+    let priority = document.querySelector('input[name="priority-radio-btn"]:checked')
+    if (taskName.value = '' || taskName.value == 'Task name') {
+        taskName.classList.add('empty-field');
+    }
+    if (datetime.value = '' || datetime.value == 'Task name') {
+        datetime.classList.add('empty-field');
+    }
+}
 export default getFormData;
