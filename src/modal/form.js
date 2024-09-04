c18944bc991701adbdf0fe08c4f2ed7b7b41fff8
wtwo1529 <003axelw@gmail.com>
2024-09-05 03:40:02 +0800

    Fix bugs corresponding to the remaining code using the removed radioInput class

--- src/modal/form.js
+++ src/modal/form.js
@@ -5,7 +5,6 @@ class Form {
     constructor(fetchData, modalRoot, modal, openModalBtns, formElement, inputElements, nonRadioInputs, radioInputs, listHeader) {
         this.formHandler = new FormHandler(fetchData, modalRoot, modal, openModalBtns, formElement, inputElements, nonRadioInputs, listHeader);
         this.formValidator = new FormValidator(nonRadioInputs, inputElements);
-        this.radioInputs = new RadioInput(radioInputs);
     }
 
 }
